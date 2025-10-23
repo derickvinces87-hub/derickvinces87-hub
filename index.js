16b104ddd75cf1559b19c86a93027295adcec148
derickvinces87-hub <derickvinces87@gmail.com>
2025-10-23 00:55:44 -0500

    Agregué código

---
+++ index.js
@@ -0,0 +1,9 @@
+function numerosPares(){
+let num=parseInt(prompt("Ingrese un numero: "));
+if (num%2===0){
+    console.log("El numero es par");
+}else{
+    console.log("El numero es impar");
+}
+}
+numerosPares();
