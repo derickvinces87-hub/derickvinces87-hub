30447a02b7d8e265bc11f0583093b023cadf2945
derickvinces87-hub <derickvinces87@gmail.com>
2025-11-09 17:59:24 -0500

    Guia Practica #3

---
+++ Guia Practica 3/index.js
@@ -0,0 +1,9 @@
+//Este archivo a sido realizado por: Derick Lizandro Vinces Ronquillo.
+//Carrera: Ingnieria de software.
+//Universidad: Universidad Estatal de Milagro (UNEMI).
+//Asignatura: Algoritmo y logica de programacion.
+//Paralelo: B1
+//Docente: MSc. Daniel Vera.
+//Guia practica #3; Semana 10.
+//Fecha de creacion: 09/11/2025
+//Fecha entrega: 28/10/2025
